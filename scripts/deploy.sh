b57f2ab062dd9ee163a2cb620eacd9c2591b57db
Josh Johnson <josh@sdfcloud.co>
2024-09-17 13:59:32 +0000

    Updating docs for build $LIB_VERSION

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # get version from composer.json
-LIB_VERSION="$(cat composer.json | tr { '\n' | tr , '\n' | tr } '\n' | grep "version" | awk  -F'"' '{print $4}')"
+LIB_VERSION="$(cat composer.json | tr { '\n' | tr , '\n' | tr } '\n' | grep "version" | awk  -F'"' '{print $4}')";
 
 # update docs
-composer build:docs
-DOCS_STATUS="$(git status)"
-echo $DOCS_STATUS
+echo "Building api docs"...;
+php phpDocumentor.phar --defaultpackagename=Sdfcloud --ignore=**/*.TestSuite.php run -d src -t docs/api;
+
+# commit docs if change is present
+GIT_STATUS="$(git status)";
+#if [[ "$GIT_STATUS" == *"docs/api"* ]]; then
+  echo "Updating docs for build $LIB_VERSION";
+  git add .
+  git commit -m 'Updating docs for build $LIB_VERSION'
+  git push origin master
+#fi
 
 git tag ${LIB_VERSION}
 git push origin ${LIB_VERSION}
